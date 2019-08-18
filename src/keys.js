c8765f8f11abfa76024f5c897086f7c072d4500d
Diego Marcillo <da>
2019-08-18 01:39:11 -0500

    Conexion db

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
      database:{
-          host: 'localhost',
+          host: '127.0.0.1',
           user: 'root',
-          password: '$root',
+          password: 'root',
           database: 'myapp'
      }
 }
--- src/routes/links.js
+++ src/routes/links.js
@@ -4,8 +4,18 @@ router.get('/add',(req,res)=>{
      res.render('links/add')
 });
 
-router.post('/add',(req,res)=>{
-     res.send('Enviado');
+router.post('/add', async (req,res)=>{
+     const {titleLink, url, descripLink} = req.body;
+     const mylink = {
+          titleLink,
+          url,
+          descripLink
+     };
+     
+     await db.query('INSERT INTO links set ?',[mylink]); 
+     res.send('envio');
+     console.log('llego');
+     
 });
 
 
